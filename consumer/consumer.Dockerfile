668621b3ff604e6b537ab559d4d847e38c131609
Marko Selendic <Marko.Selendic@fer.hr>
2024-01-14 16:23:51 +0100

    Added consumer workflow

--- consumer/consumer.Dockerfile
+++ consumer/consumer.Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.12.1-slim
 WORKDIR /app
-COPY requirements.txt .
+COPY consumer/requirements.txt .
 RUN pip install -r requirements.txt
-COPY consumer.py .
+COPY consumer/consumer.py .
 CMD ["python", "consumer.py"]
--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,36 +1,32 @@
 import asyncio
-import os
 
 from azure.eventhub.aio import EventHubConsumerClient
 
-EVENT_HUB_CONNECTION_STR = str(os.environ["EVENT_HUB_CONNECTION_STR"])
-EVENT_HUB_NAME = str(os.environ["EVENT_HUB_NAME"])
+EVENT_HUB_CONNECTION_STR = "evh-redditapi-dev-we-tpiuolab-ms-01"
+EVENT_HUB_NAME = "Endpoint=sb://evhns-dev-hr-tpiuo-ms-01.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=WOVOO4BrzTTqxF9K+AlYbpOEFgqffjVU9+AEhA0MxUc="
 
 
 async def on_event(partition_context, event):
     # Print the event data.
-    print(
-        'Received the event: "{}" from the partition with ID: "{}"'.format(
-            event.body_as_str(encoding="UTF-8"), partition_context.partition_id
-        )
-    )
+    print(event.body_as_str(encoding="UTF-8"), partition_context.partition_id)
 
     # Update the checkpoint so that the program doesn't read the events
     # that it has already read when you run it next time.
     await partition_context.update_checkpoint(event)
 
-async def consume():
 
+async def consume():
     # Create a consumer client for the event hub.
     client = EventHubConsumerClient.from_connection_string(
         EVENT_HUB_CONNECTION_STR,
         consumer_group="$Default",
-        eventhub_name=EVENT_HUB_NAME
+        eventhub_name=EVENT_HUB_NAME,
     )
     async with client:
         # Call the receive method. Read from the beginning of the
         # partition (starting_position: "-1")
         await client.receive(on_event=on_event, starting_position="-1")
+
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
